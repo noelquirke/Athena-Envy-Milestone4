dbb8443ccba3cea3fe1ae64fb2382efd9f386536
Noel Quirke <noelquirke900@hotmail.com>
2020-08-25 21:30:57 +0000

    Editing about.html, code clean up

--- cart/views.py
+++ cart/views.py
@@ -6,16 +6,16 @@ import requests
 
 
 def view_cart(request):
-    """A View that renders the cart contents page"""
+    # A View that renders the cart contents page
     return render(request, "cart.html")
 
 
 def add_to_cart(request, id):
-    """Add a quantity of the specified product to the cart"""
+    # Add a quantity of the specified product to the cart
     quantity = 1
 
     cart = request.session.get('cart', {})
-    cart.clear()
+    cart.clear()  # Only allows 1 item to be booked per checkout
     cart[id] = cart.get(id, quantity)
 
     request.session['cart'] = cart
--- products/models.py
+++ products/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
+    long_description = models.TextField(default="")
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
 
